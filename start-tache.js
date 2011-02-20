dee5f2350b63e4a351449b3ceb1f747654dfa53f
Owen Smith <owen.rl.smith@gmail.com>
2011-02-20 16:39:33 +0000

    Minimal module structure (abandon www folder in favour of tache.io folder) with top-level bootstrap script.

---
+++ start-tache.js
@@ -0,0 +1,3 @@
+var tache = require("./tache.io");
+
+tache.init();
---
+++ tache.io/tache.js
@@ -0,0 +1,9 @@
+var http  = require('http'),
+    https = require('https'),
+    sys   = require('sys'),
+    fs    = require('fs');
+
+
+exports.init = function(){
+
+}

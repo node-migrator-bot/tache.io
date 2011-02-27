d1e4d55eecba9076a44bd5767a5367686b62ceaf
Owen Smith <owen.rl.smith@gmail.com>
2011-02-27 15:43:07 +0000

    More tests

---
+++ tests/broken.endpoint.js
@@ -0,0 +1,3 @@
+var Endpoint = require('tache.io').Endpoint;
+
+module.exports = exports = new Endpoint("This param is a string not a function. This endpoint won't load");
--- tests/server.tests.js
+++ tests/server.tests.js
@@ -1,15 +1,29 @@
 var tache = require("tache.io"),
     assert = require('assert');
 
-var server = tache.init('', false);
+var server = tache.init('test-config.json',__dirname, false);
+
+//TODO: would be nice to have these in a config somewhere, available globally and called by key
+// (like an internationalization file). Would avoid having to duplicate them exactly here.
+var responses = {
+  bad_endpoint:"The required endpoint was not found or is unavailable\n"
+};
 
 module.exports = {
 
   "Unknown endpoint 404":function(){
     assert.response(server, {
-      url:"/qwertyuiop/asdfghjkl/http://www.google.com"
+      url:"/qwertyuiop/asdfghjkl/http://www.example.com"
     },{
-      body:"The required endpoint was not found or is unavailable\n"
+      body:responses.bad_endpoint
+    });
+  },
+  
+  "Broken endpoint 404":function(){
+    assert.response(server, {
+      url:"/broken.endpoint/http://www.example.com"
+    },{
+      body:responses.bad_endpoint
     });
   }
 

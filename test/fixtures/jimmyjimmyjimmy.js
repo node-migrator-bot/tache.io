826b1fb41f2b7c8c1898c53cf76f640caf372e22
Owen Smith <owen.rl.smith@gmail.com>
2011-04-07 08:52:53 +0100

    Clean up: fix comments, remove hardcoded paths

--- test/fixtures/jimmyjimmyjimmy.js
+++ test/fixtures/jimmyjimmyjimmy.js
@@ -2,7 +2,7 @@ var im      = require('imagemagick'),
     fs      = require('fs'),
     Canvas  = require('canvas'),
     Image   = Canvas.Image, 
-    ccv     = require('/Users/owen/projects/tache.io/test/fixtures/ccv.js'),
+    ccv     = require('./ccv.js'),
     crypto  = require('crypto'),
     util    = require('util');
     
@@ -15,10 +15,8 @@ module.exports.emits = 'binary';
 module.exports.do = function(headers, input) {
   var self = this;
   this.response.ttl = '30s';
-  //this.response.type = headers['Content-type'] || 'image/jpeg';
-  this.response.type = 'image/png';
   
-  //indentify the image
+  //identify the image to get width/height
   im.identify({data:input},function(err,info){
     if (err) {
       console.log(err);
@@ -28,7 +26,7 @@ module.exports.do = function(headers, input) {
       , ctx = canvas.getContext('2d')
       , tmpImg = new Image;
     
-    //pass the recieved data through imagemagick to force PDF
+    //pass the recieved data through imagemagick to force PNG
     im.resize({
       srcData: input,
       format:'png',
@@ -38,8 +36,6 @@ module.exports.do = function(headers, input) {
       
       //when the remote image has been pnged and loaded, do the ccv comp and draw the result
       tmpImg.onload = function(){
-        console.log('image loaded');
-        //return;
         
         try{
           ctx.drawImage(tmpImg,0,0);
@@ -52,11 +48,10 @@ module.exports.do = function(headers, input) {
             "interval" : 5,
             "min_neighbors" : 1 });
           
-          console.log('dection finished');
+          console.log('detection finished, ' + comp.length + ' faces found');
           //If we got here, we've probably won. Set the headers, start streaming after we've drawn what we want
           
           var done = function() {
-            console.log('comp done');
             var stream = canvas.createPNGStream();
             util.pump(stream, self.response, function(err) {
               if(err)
@@ -64,7 +59,7 @@ module.exports.do = function(headers, input) {
               self.response.end();
             });
           };
-          
+          //write the header ourself, because we'll be streaming (helper methods currently don't support streaming)
           self.response.writeHead(200, {
             "Content-Type": "image/png"
           });
@@ -76,8 +71,8 @@ module.exports.do = function(headers, input) {
           {
             drawJimmy = true;
             jimmy = new Image();
-            //from experimentation with the original jimmy pic:
             
+            //from experimentation with self-application against the original jimmy pic:
             var scale = 1.4  // how much larger to make the cutout (than the bounding box width)
             
             jimmy.onload = function() {
@@ -93,8 +88,6 @@ module.exports.do = function(headers, input) {
                 //but for strict scaling correctness I base it all on one measurement (width), and figure out
                 //the height based on the cutout's original w/h ratio
                 
-                
-                
                 ctx.drawImage(jimmy,
                   faceData.x + (faceData.width / 2) - (width / 2),
                   faceData.y + (faceData.height / 2) - (height /2),
@@ -102,21 +95,12 @@ module.exports.do = function(headers, input) {
                   );
                 console.log('drawn face '+i);
               }
-              console.log('finished drawing jimmy');
               done();
             };
             //load the jimmy overlay img.
-            jimmy.src = "/Users/owen/projects/tache.io/test/fixtures/jimmy_overlay.png";
+            jimmy.src = __dirname + "/jimmy_overlay.png";
             
           }
-          
-          /*
-          ctx.lineWidth = 2;
-          ctx.strokeStyle = "rgba(255,0,0,.50)";
-          // draw detected area
-          for (var i = 0; i < comp.length; i++)
-            ctx.strokeRect(comp[i].x, comp[i].y, comp[i].width, comp[i].height);  
-          */
           
           if(!drawJimmy) done();
             
@@ -126,13 +110,9 @@ module.exports.do = function(headers, input) {
         }
       };
       
-      console.log('image loading..');
-      console.log(stdout.length);
-      console.log(Buffer.byteLength(stdout));
-      
       try
       {
-        //load the newly-forced PDF straight into the image elem
+        //load the newly-forced PNG straight into the image elem
         tmpImg.pngData = stdout;
       } catch(e) {
         console.log(e);
@@ -141,23 +121,5 @@ module.exports.do = function(headers, input) {
       
     });
   });
-  //construct a fake canvas to dump out image into
-  
-  
-  //spin up the CCV routines
-  
-  
-  //read back the canvas data and reply.
-  
-  /*
-  im.resize({
-    srcData: data,
-    width:'100%',
-    customArgs:['-flip']
-  }, function(err, stdout, stderr){
-    if (err) throw err
-    self.reply(stdout);
-  });
-  */
 };
 

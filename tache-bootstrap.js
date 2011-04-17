00f942754d832b3720a00539702a5a533b9308a5
Owen Smith <owen.rl.smith@gmail.com>
2011-04-17 19:03:09 +0100

    rename bootstrap script, get rid of redundant equivalent

--- tache-bootstrap.js
+++ tache-bootstrap.js
@@ -1,6 +1,5 @@
 /*
-Trivial bootstrapper to kickstart tache with a
-config file specified on the command line.
+Trivial bootstrapper to kickstart tache from the command line.
 
 Intended for local use during development, but also 
 as an example of a minimal script to get tache running.
